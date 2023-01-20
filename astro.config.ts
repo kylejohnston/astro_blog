6bde5781590515548cc59722343b4bdb779cdc23
Kyle Johnston <flow14@gmail.com>
2023-01-20 01:57:21 -0600

    Updated site value

--- astro.config.ts
+++ astro.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
 			wrap: true,
 	},
 	},
-	site: "https://kylejohnston.xyz/writing/",
+	site: "https://kylejohnston.xyz/",
   integrations: [tailwind(), image(), sitemap()],
 });
